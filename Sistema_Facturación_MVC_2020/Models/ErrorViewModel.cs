4c88362c6f5b27be4762ae42031379591b20add3
Diego De La Torre <diego.dlt29@hotmail.com>
2020-08-29 21:05:53 -0500

    PonerSistema

---
+++ Sistema_Facturación_MVC_2020/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Sistema_Facturación_MVC_2020.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
